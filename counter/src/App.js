a2416fc52526a67374b71f0256d71aa9675b7d34
hydrol0x <mrjacobry@gmail.com>
2022-10-17 12:22:19 -0400

    counter dir

---
+++ counter/src/App.js
@@ -0,0 +1,31 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className="App">
+      <h1> Counter </h1>
+      <p> {count} </p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default App;
