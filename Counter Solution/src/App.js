617910ac42ec7bc0849671e0cd00242d9910db20
hydrol0x <mrjacobry@gmail.com>
2022-10-22 10:40:44 -0400

    Added functionality for challenge 6. Will display a '100' emoji when you reach 100

--- Counter Solution/src/App.js
+++ Counter Solution/src/App.js
@@ -5,14 +5,37 @@ import CounterAdvanced from "./components/counterAdvanced";
 import MyCounter from "./components/myCounter";
 
 function App() {
+  // This is the most basic version that starts at a value of 0
   const [count, setCount] = useState(0);
+
+  // This is the solution code for challenge 4.
+  // If you would like to see how it works, uncomment it and comment out the other useStates
+  // const init = 10
+  // const [count, setCount] = useState(init);
+
+  // This is the solution code for challenge 5.
+  // If you would like to see how it works, uncomment it and comment out the other useStates
+  // const max = 100;
+  // let init = Math.floor(Math.random() * max);
+  // const [count, setCount] = useState(init);
 
   const increment = () => {
     setCount(count + 1);
   };
 
+  // This function is used for challenege 1.
+  const incrementBy5 = () => {
+    setCount(count + 5);
+  };
+
   const decrement = () => {
-    setCount(count - 1);
+    // Challenge 3.
+    // If the count is 0 then keep it at 0 otherwise decrement.
+    if (count === 0) {
+      setCount(0);
+    } else {
+      setCount(count - 1);
+    }
   };
 
   const reset = () => {
@@ -24,9 +47,20 @@ function App() {
       {/* <CounterBasic count={count} increment={increment} /> */}
       {/* <CounterAdvanced count={count} increment={increment} decrement={decrement} reset={reset} /> */}
 
-      <CounterAdvanced count={count} increment={increment} decrement={decrement} reset={reset} />
+      {/* <CounterAdvanced
+        count={count}
+        increment={increment}
+        decrement={decrement}
+        reset={reset}
+      /> */}
 
-      {/* <MyCounter />       */}
+      <MyCounter
+        count={count}
+        increment={increment}
+        incrementBy5={incrementBy5}
+        decrement={decrement}
+        reset={reset}
+      />
     </div>
   );
 }
--- Counter Solution/src/components/myCounter.js
+++ Counter Solution/src/components/myCounter.js
@@ -1,6 +1,22 @@
-// TODO: implement all of the challenges
-function MyCounter({}) {
-  return 0;
+function MyCounter({ count, increment, incrementBy5, decrement, reset }) {
+  // prettier-ignore
+  return (
+    <div className="MyCounter">
+      <h1> My Counter </h1>
+
+      {/* Challenge 6. */}
+      <h1 className='hundredHeading'>{'💯'.repeat(Math.floor(count/100))}</h1> 
+
+      {/* Challenge 2. */}
+      <p> The current count is: {count} </p> 
+      <button onClick={incrementBy5}>Increment by 5</button>
+
+      {/* Challenge 1. */}
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
 }
 
 export default MyCounter;
