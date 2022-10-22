1ca706e81615dbf359943700952dad15d04c232a
hydrol0x <mrjacobry@gmail.com>
2022-10-21 22:07:55 -0400

    Added prompts to implement code for advanced counter

--- Counter/src/components/counterAdvanced.js
+++ Counter/src/components/counterAdvanced.js
@@ -1,11 +1,20 @@
+// This is the advanced counter. It is similar to the basic counter, but we will add some more functionality.
+
 function CounterAdvanced({ count, increment, decrement, reset }) {
   return (
     <div className="CounterAdvanced">
-      <h1> Counter </h1>
-      <p> {count} </p>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={reset}>Reset</button>
+      <h1> Counter Advanced </h1>
+      {/* Add a display for the count: */}
+      {/* Write code here: */}
+
+      {/* Add a button that increments the counter, just like in the basic example */}
+      {/* Write code here: */}
+
+      {/* Add a button that decrements the counter, or subtracts 1 from it: */}
+      {/* Write code here: */}
+
+      {/* Add a button that resets the counter to 0: */}
+      {/* Write code here: */}
     </div>
   );
 }
