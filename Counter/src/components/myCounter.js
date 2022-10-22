9303bd204d4b0a98c8ae1b53883a71393cd60cb5
hydrol0x <mrjacobry@gmail.com>
2022-10-22 09:16:38 -0400

    edited instruction slightly for clarity

--- Counter/src/components/myCounter.js
+++ Counter/src/components/myCounter.js
@@ -6,7 +6,7 @@
 // Some challenges:
 // 1. Create a button that increments or decrements by different amounts other than 1
 // 2. Display some text other than just the 'count' variable
-// 3. Don't let the counter go negative (maybe reset it to 0 if you are going to go to a negative number)
+// 3. Don't let the counter go negative (hint: set it to 0 if you are going to decrement below 0)
 // 4. Initialize the counter with a number other than 0
 // 5. Initialize the counter with a random number each time
 // 6. Change the text once you get past a milestone- like a count of 100
