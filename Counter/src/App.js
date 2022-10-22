3fa9d212404f4432f552ca8c4311ee0ef01e6bbf
hydrol0x <mrjacobry@gmail.com>
2022-10-21 22:28:32 -0400

    Added MyCounter comments and prompts

--- Counter/src/App.js
+++ Counter/src/App.js
@@ -23,10 +23,13 @@ function App() {
 
   return (
     <div className="App">
+      {/* The basic counter component. If this line is uncommented, you will see a counter with one increment button on your web page. */}
       <CounterBasic count={count} increment={increment} />
 
-      {/* When you are ready to use CounterAdvanced, comment the above line out and uncomment the below line (make sure to fill in all of the arguments) */}
+      {/* When you are ready to use CounterAdvanced, comment the above line out and uncomment the below line (make sure to fill in all of the arguments)! */}
       {/* <CounterAdvanced count={} increment={} decrement={} reset={} /> */}
+
+      {/* When you are ready to use MyCounter, comment the above lines and add your MyCounter comopnent below! */}
     </div>
   );
 }
--- Counter/src/components/myCounter.js
+++ Counter/src/components/myCounter.js
@@ -1,1 +1,13 @@
 // LEVEL 3 - MAKE YOUR OWN
+
+// This is the end of your very first introduction to writing React code. Now you have to implement the comopnent yourself. Start with the boilerplate and go from there.
+// Try to challenge yourself and go beyond even the three buttons you implemented in the last component.
+
+// Some challenges:
+// 1. Create a button that increments or decrements by different amounts other than 1
+// 2. Display some text other than just the 'count' variable 
+// 3. Don't let the counter go negative (maybe reset it to 0 if you are going to go to a negative number)
+// 4. Initialize the counter with a number other than 0 
+// 5. Initialize the counter with a random number each time
+
+// Your code goes here:
